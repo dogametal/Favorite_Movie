e1dd07f7b3c884fd854b6e5c19bf94da148fee56
dogametal <dogametal@hotmail.com>
2021-01-09 06:13:37 -0300

    Controlling backdrop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,8 +6,28 @@ const addMovieModal = document.getElementById('add-modal');
 const startMovieButton = document.querySelector('header button');
 //const startMovieButton = document.querySelector('header').lastElementChild;
 
+const backdrop = document.getElementById('backdrop');
+//const backdrop = document.body.firstElementChild;
+
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible');
+};
+
 const toggleMovieModal = () => {//function model(){}
     addMovieModal.classList.toggle('visible'); //this method classList.toggle avoid error when it´s visible
+    toggleBackdrop();
 }; 
 
+const cancelAddMovie = () => {
+    toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+    toggleMovieModal();
+};
+
 startMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
