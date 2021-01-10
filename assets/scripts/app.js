664afa206818a6475d16a3510424ccd6e3dfa541
dogametal <dogametal@hotmail.com>
2021-01-10 06:32:41 -0300

    Create List of Elements

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -15,12 +15,40 @@ const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
 //const userInputs = addMovieModal.getElementsByTagName('input');
 
+const entryTextSection = document.getElementById('entry-text');
+
 const movies = [];
 
 const clearMovieInput = () => {
 	for (const usrInput of userInputs){
 		usrInput.value = '';
 	}
+};
+
+const updateUI = () => {
+	if(movies.lengh ===0){
+		entryTextSection.style.display = 'block';
+	}
+	else{
+		entryTextSection.style.display = 'none';
+	}
+	
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+	const newMovieElement = document.createElement('li');
+	newMovieElement.className = 'movie-element';
+	newMovieElement.innerHTML = ` 
+	<div class="movie-element_image">
+		<img src="${imageUrl}" alt="${title}">
+	</div>
+	<div class="movie-element_info">
+		<h2>${title}</h2>
+		<p>${rating}/5 stars</p>
+	</div">
+	`;
+	const listRoot = document.getElementById('movie-list');
+	listRoot.append(newMovieElement);
 };
 
 const toggleBackdrop = () => {
@@ -59,6 +87,8 @@ const addMovieHandler = () => {
     console.log(movies);
     toggleMovieModal();
     clearMovieInput();
+    renderNewMovieElement(newMovies.title, newMovies.image, newMovies.rating);
+    updateUI();
 };
 
 const backdropClickHandler = () => {
